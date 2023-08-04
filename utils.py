c22c8d91ee9c5c63245703971640871455f928f7
Amy Wen <wen.amy@gmail.com>
2023-08-04 11:22:54 -0400

    update utils to loop through playlist with more than 100 tracks

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 # Import packages
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import yaml
 import pandas as pd
+import spotipy
+import yaml
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # Load client secrets (dict with client_id and client_secret)
 client_secrets = yaml.load(open('secrets/client.yml').read(), yaml.FullLoader)
@@ -10,6 +10,7 @@ client_secrets = yaml.load(open('secrets/client.yml').read(), yaml.FullLoader)
 # API authentication
 client_credentials_manager = SpotifyClientCredentials(**client_secrets)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 # Function to create df with information from tracks output
 def get_track_df(tracks):
@@ -34,20 +35,22 @@ def get_track_df(tracks):
             'explicit': track['explicit'],
             'popularity': track['popularity'],
             'artist_pop': artist_info['popularity'],
-            'artist_genres': artist_info['genres']
+            'artist_genres': artist_info['genres'],
         }
 
         try:
             # Audio info
-            audio_info = sp.audio_features(track['id'])[0]  # "danceability", "energy", "key", "loudness", "mode", "speechiness", "instrumentalness", "liveness", "valence", "tempo", "duration_ms", "time_signature"
+            audio_info = sp.audio_features(track['id'])[
+                0
+            ]  # "danceability", "energy", "key", "loudness", "mode", "speechiness", "instrumentalness", "liveness", "valence", "tempo", "duration_ms", "time_signature"
 
             # Add audio info
             playlist_features.update(audio_info)
-            
+
             # Concat the dfs
             track_df = pd.DataFrame([playlist_features])
 
-        except:
+        except Exception:
             continue
 
         if i == 0:
@@ -62,7 +65,7 @@ def get_track_df(tracks):
 def analyze_playlist(playlist_ids):
     if isinstance(playlist_ids, str):
         playlist_ids = [playlist_ids]
-    
+
     for i, playlist_id in enumerate(playlist_ids):
         # Loop through every track in the playlist, extract features and append the features to the playlist df
         results = sp.playlist(playlist_id, fields="tracks,next")
